3f5d7da17bd40e74c7cedd3ea5fb02535e8954fe
unknown <frederic-batault@hotmail.fr>
2018-05-31 09:17:33 +0200

    ajout du projet au depot

---
+++ src/main/java/org/apache/struts/helloworld/action/HelloWorldAction.java
@@ -0,0 +1,43 @@
+package org.apache.struts.helloworld.action;
+
+import org.apache.struts.helloworld.model.MessageStore;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloWorldAction extends ActionSupport {
+
+	private MessageStore messageStore;
+
+	private static int helloCount = 0;
+
+	private String userName;
+
+	public MessageStore getMessageStore() {
+		return messageStore;
+	}
+
+	public int getHelloCount() {
+		return helloCount;
+	}
+
+	public void setMessageStore(MessageStore messageStore) {
+		this.messageStore = messageStore;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String execute() {
+		messageStore = new MessageStore();
+		if (userName != null) {
+			messageStore.setMessage(messageStore.getMessage() + " " + userName);
+		}
+		helloCount++;
+		return SUCCESS;
+	}
+}
